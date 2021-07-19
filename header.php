58c08286d616075365891d21ec8ca2dbbc418cc2
YAS <flod@yasbox.com>
2021-07-19 17:58:24 +0900

    fix

--- header.php
+++ header.php
@@ -12,6 +12,10 @@ $includeCSSPath = '/dist/css/' . get_post_meta(get_the_ID(), 'includeCSS', true)
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
     <!--　SEO　-->
     <meta name="description" content="<?php bloginfo('description'); ?>">
+    <!--　fonts　-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&display=swap" rel="stylesheet">
     <!--　スタイルシート　-->
     <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
