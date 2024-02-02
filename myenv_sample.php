86eb1bf02861a2498ddf1fddbb7204309b8d3c04
YAS <54887774+yasbox@users.noreply.github.com>
2024-02-02 15:01:05 +0900

    myenv.phpの雛形を追加

---
+++ myenv_sample.php
@@ -0,0 +1,14 @@
+<?php
+
+// reCAPTCHA v3 サイトキー
+define('V3_SITEKEY', '');
+// reCAPTCHA v3 シークレットキー
+define('V3_SECRETKEY', '');
+
+// メール送信設定
+define('MAIL_HOST', '');
+define('MAIL_PORT', '587');
+define('MAIL_USERNAME', '');
+define('MAIL_PASSWORD', '');
+define('MAIL_FROM_ADDRESS', '');
+define('MAIL_FROM_NAME', 'お問い合わせフォーム');
